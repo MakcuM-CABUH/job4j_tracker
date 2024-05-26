7bd1e756c5e35405c3c21d3c4dd6e22a94a7b3b9
MakcuM-CABUH <job4j@mail.ru>
2024-05-26 22:25:24 +0500

    проверка Checkstyle

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -1,18 +1,20 @@
 package ru.job4j.checkstyle;
 
 public class Broken {
-    public static final String NEW_VALUE = "";
     private int sizeOfEmpty = 10;
-    public String surname = "";
-    public String Name = "";
+    public String surname;
+    public String name;
+    public static final String NEW_VALUE = "";
 
-    void echo() {
-    }
+    void echo() { }
+
     void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
-        void method(int a; int b; int c) { }
     }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) { }
+
     void broken() { }
 }
