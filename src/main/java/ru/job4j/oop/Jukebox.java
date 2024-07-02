b67f86a9b3b11c21b37281fdc2c7bf851d7c15c7
MakcuM-CABUH <job4j@mail.ru>
2024-07-02 16:28:32 +0500

    Вызов метода объекта

---
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,21 @@
+package ru.job4j.oop;
+
+public class Jukebox {
+
+    public void music(int position) {
+        if (position == 1) {
+            System.out.println("Пусть бегут неуклюже пешеходы по лужам...");
+        } else if (position == 2) {
+            System.out.println("Глазки закрывай, баю-бай...");
+        } else {
+            System.out.println("Песня не найдена");
+        }
+    }
+
+    public static void main(String[] args) {
+        Jukebox box = new Jukebox();
+        box.music(1);
+        box.music(2);
+        box.music(12);
+    }
+}
