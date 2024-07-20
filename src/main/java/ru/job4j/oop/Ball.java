f77cf8c3a00ae4bb42336fdce7877d6c61564f58
MakcuM-CABUH <job4j@mail.ru>
2024-07-20 12:50:06 +0500

    1.6. Взаимодействие объектов [#166087 #505295]

--- src/main/java/ru/job4j/oop/Ball.java
+++ src/main/java/ru/job4j/oop/Ball.java
@@ -3,5 +3,10 @@ package ru.job4j.oop;
 public class Ball {
 
     public void tryRun(boolean condition) {
+        if (!condition) {
+            System.out.println("Ему встретился Заяц. Колобок запел ему песенку. Заяц заслушался. Колобок побежал дальше.");
+        } else {
+            System.out.println("Ему встретилась Лиса. Колобок ей тоже запел песенку. Но Лиса съела Колобка. Конец сказки");
+        }
     }
 }
