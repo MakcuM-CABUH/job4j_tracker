c383f363ab91b87ca0a24763e2bb3314468f0b0c
MakcuM-CABUH <job4j@mail.ru>
2024-08-14 12:22:22 +0500

    2.0. Рефакторинг - Расстояние между точками. [#122587]

---
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.withPrecision;
+
+class PointTest {
+
+    @Test
+    void when00To20Then2() {
+        double expected = 2.0;
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double rsl = a.distance(b);
+        assertThat(rsl).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when20To13Then3dot16() {
+        double expected = 3.16;
+        Point a = new Point(2, 0);
+        Point b = new Point(1, 3);
+        double rsl = a.distance(b);
+        assertThat(rsl).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when00To13Then3dot16() {
+        double expected = 3.16;
+        Point a = new Point(0, 0);
+        Point b = new Point(1, 3);
+        double rsl = a.distance(b);
+        assertThat(rsl).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when02To27Then5dot39() {
+        double expected = 5.39;
+        Point a = new Point(0, 2);
+        Point b = new Point(2, 7);
+        double rsl = a.distance(b);
+        assertThat(rsl).isEqualTo(expected, withPrecision(0.01));
+    }
+}
