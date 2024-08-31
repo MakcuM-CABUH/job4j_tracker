c58a32bdea22490162267b79c547e19b4e3d0e1b
MakcuM-CABUH <job4j@mail.ru>
2024-08-31 16:02:03 +0500

    8. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/inheritance/Item.java
@@ -0,0 +1,43 @@
+package ru.job4j.inheritance;
+
+import java.time.LocalDateTime;
+
+public class Item {
+    private int id;
+    private String name;
+
+    private LocalDateTime created = LocalDateTime.now();
+
+    public Item() {
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocalDateTime(String created) {
+        this.created = LocalDateTime.parse(created);
+        return created;
+    }
+}
