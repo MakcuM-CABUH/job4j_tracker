9cff2db3dcaffd4e5f1bfb12d862bb4759e15876
MakcuM-CABUH <job4j@mail.ru>
2024-09-24 12:30:09 +0500

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,40 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+
+        Book kolobok = new Book("Kolobok", 5);
+        Book cleanCode = new Book("Clean code", 500);
+        Book hell = new Book("Hell", 110);
+        Book tales = new Book("Tales", 300);
+
+        Book[] books = new Book[4];
+        books[0] = kolobok;
+        books[1] = cleanCode;
+        books[2] = hell;
+        books[3] = tales;
+
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            System.out.println(book.getName() + " - " + book.getPages());
+        }
+        System.out.println(" ");
+        Book temp = books[0];
+        books[0] = books[3];
+        books[3] = temp;
+
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            System.out.println(book.getName() + " - " + book.getPages());
+        }
+        System.out.println(" ");
+
+        for (int index = 0; index < books.length; index++) {
+            Book book = books[index];
+            if (book.getName().equals("Clean code")) {
+                System.out.println(book.getName() + " - " + book.getPages());
+
+            }
+        }
+    }
+}
