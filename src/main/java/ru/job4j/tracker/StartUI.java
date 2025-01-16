d9180fba4968de73070c02d4db16c1708bf0b034
MakcuM-CABUH <job4j@mail.ru>
2025-01-16 15:08:58 +0500

    3.6. Реализация класса StartUI. Вывод заявок по имени [#500749]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -52,6 +52,18 @@ public class StartUI {
                 int id = Integer.parseInt(scanner.nextLine());
                 Item item = tracker.findById(id);
                 System.out.println(item != null ? item : "Ошибка поиска заявки.");
+            } else if (select == 5) {
+                System.out.println("===Поиск заявки по имени)===");
+                System.out.print("Enter NAME: ");
+                String name = scanner.nextLine();
+                Item[] items = tracker.findByName(name);
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Ошибка поиска. Заявка с именем \"" + name + "\" не найдена");
+                }
             } else if (select == 6) {
                 run = false;
             }
