fd5186a5b470ebc1a44cbcc8709bf904e781bc9b
MakcuM-CABUH <job4j@mail.ru>
2026-07-10 20:44:44 +0500

    0. Сортировка [#10096]

--- src/main/java/ru/job4j/collection/JobSorter.java
+++ src/main/java/ru/job4j/collection/JobSorter.java
@@ -8,8 +8,8 @@ public class JobSorter {
     public static void main(String[] args) {
         List<Job> jobs = Arrays.asList(
                 new Job("Fix bugs", 4),
-                new Job("Impl task", 2),
-                new Job("Reboot server", 1)
+                new Job("Reboot server", 1),
+                new Job("Impl task", 2)
         );
         System.out.println(jobs);
 /*
